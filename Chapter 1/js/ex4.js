278791ed76155cbf4a40130ad134997ef41ca692
Emmanuel <emmanuel.mejia01@student.csulb.edu>
2024-12-18 16:01:19 -0800

    initialize

---
+++ Chapter 1/js/ex4.js
@@ -0,0 +1,11 @@
+console.log("Emmanuel Mejia");
+console.log("30")
+
+console.log(6 + 3);
+console.log(6 - 3);
+console.log(6 * 3);
+console.log(6 / 3);
+
+console.log(4 + 20);
+console.log("4 + 20");
+console.log("4" + "20");
