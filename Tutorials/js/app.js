ec9540a8973c08b92de3d5d6d224565242a3e494
Emmanuel <emmanuel.mejia01@student.csulb.edu>
2025-02-10 19:00:39 -0800

    test

--- Tutorials/js/app.js
+++ Tutorials/js/app.js
@@ -1,11 +1,11 @@
 // wow much JavaScript here
 var pics = [
-  "/IS445/Tutorials/imgs/kitty_bed.JPG", //0
-  "/IS445/Tutorials/imgs/kitty_basket.JPG", //1
-  "/IS445/Tutorials/imgs/kitty_laptop.JPG", //2
-  "/IS445/Tutorials/imgs/kitty_door.JPG", //3
-  "/IS445/Tutorials/imgs/kitty_sink.JPG", //4
-  "/IS445/Tutorials/imgs/kitty_wall.JPG" //5
+  "imgs/kitty_bed.JPG", //0
+  "imgs/kitty_basket.JPG", //1
+  "imgs/kitty_laptop.JPG", //2
+  "imgs/kitty_door.JPG", //3
+  "imgs/kitty_sink.JPG", //4
+  "imgs/kitty_wall.JPG" //5
 ];
 
 var btn = document.querySelector("button");
