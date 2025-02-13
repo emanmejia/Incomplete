f5590b02deb28eb0f007e703001826c0f0c18af2
Emmanuel <emmanuel.mejia01@student.csulb.edu>
2025-02-13 15:44:51 -0800

    1.2.31

---
+++ 7/js/ex1.js
@@ -0,0 +1,10 @@
+const movies = {
+  movie1: "The Wolf of Wall Street",
+  movie2: "Zootopia",
+  movie3: "Babysitting",
+  // ...
+};
+
+
+const movies2 = ["The Wolf of Wall Street", "Zootopia", "Babysitting"];
+console.log(movies.length); // 3
