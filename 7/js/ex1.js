1b9fc0b900c20b12d874a84da7c37f8923ad8ce7
Emmanuel <emmanuel.mejia01@student.csulb.edu>
2025-02-13 18:09:33 -0800

    1.3.03

--- 7/js/ex1.js
+++ 7/js/ex1.js
@@ -1,10 +1,5 @@
-const movies = {
-  movie1: "The Wolf of Wall Street",
-  movie2: "Zootopia",
-  movie3: "Babysitting",
-  // ...
-};
-
-
-const movies2 = ["The Wolf of Wall Street", "Zootopia", "Babysitting"];
-console.log(movies.length); // 3
+const movies = ["The Wolf of Wall Street", "Zootopia", "Babysitting"];
+console.log(movies[0]); // "The Wolf of Wall Street"
+console.log(movies[1]); // "Zootopia"
+console.log(movies[2]); // "Babysitting"
+console.log(movies[3]); // UNDEFINED
