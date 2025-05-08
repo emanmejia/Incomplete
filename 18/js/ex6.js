b9575fc04024d15208af1a20f2743c87a48ac820
emanejia <emanmejia@gmail.com>
2025-05-07 22:42:08 -0700

    18

--- 18/js/ex6.js
+++ 18/js/ex6.js
@@ -1,5 +1,24 @@
-// Show some info about the first form element
-const formElement = document.querySelector("form");
-console.log(`Number of fields: ${formElement.elements.length}`); // 10
-console.log(formElement.elements[0].name);       // "username"
-console.log(formElement.elements.password.type); // "password"
+const timerDisplay = document.getElementById('timer');
+const startButton = document.getElementById('startButton');
+let intervalId;
+let seconds = 0;
+let isRunning = false;
+
+function updateTimer() {
+    seconds++;
+    timerDisplay.textContent = `${seconds} seconds`;
+}
+
+function startChronometer() {
+    if (!isRunning) {
+        intervalId = setInterval(updateTimer, 1000);
+        startButton.textContent = 'Stop';
+        isRunning = true;
+    } else {
+        clearInterval(intervalId);
+        startButton.textContent = 'Start';
+        isRunning = false;
+    }
+}
+
+startButton.addEventListener('click', startChronometer);
